f3005859f46142e95b095e41960ab2b56fcd1fd7
Samuel Giles <sam@samuelgil.es>
2018-12-03 13:04:15 +0000

    Add day one part one solution

---
+++ AdventOfCode2018/DayOne/DestinationLockGrabber.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018.DayOne
+{
+    public class DestinationLockGrabber
+    {
+        public DestinationLockGrabber(string input)
+        {
+            Input = input;
+        }
+
+        public string Input { get; }
+
+        public int ResultingFrequency()
+        {
+            List<int> inputInts = InputInts();
+            int startingFrequency = inputInts.First();
+            int finalValue = startingFrequency;
+            List<int> remainingFrequenciesToApply = InputInts().GetRange(index: 1, count: inputInts.Count()-1);
+
+            foreach (int frequencyToApply in remainingFrequenciesToApply)
+            {
+                finalValue += frequencyToApply;
+            }
+
+            return finalValue;
+        }
+
+        private List<int> InputInts()
+        {
+            InputParser inputParser = new InputParser(Input);
+            return inputParser.AsInts();
+        }
+
+        class InputParser
+        {
+            public InputParser(string inputLines)
+            {
+                InputLines = inputLines;
+            }
+
+            private string InputLines { get; }
+
+            public List<int> AsInts()
+            {
+                StringReader stringReader = new StringReader(InputLines);
+                List<int> parsedInts = new List<int>();
+
+                while (true)
+                {
+                    string line = stringReader.ReadLine();
+
+                    if (line != null)
+                    {
+                        InputLine inputLine = new InputLine(line);
+                        parsedInts.Add(inputLine.AsInt());
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return parsedInts;
+            }
+        }
+
+        class InputLine
+        {
+            public InputLine(string inputLine)
+            {
+                line = inputLine;
+            }
+
+            private string line { get; }
+
+            public int AsInt()
+            {
+                return int.Parse(line);
+            }
+        }
+    }
+}
---
+++ AdventOfCode2018/DayOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdventOfCode2018.DayOne
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText("./DayOne/DayOneInput.txt");
+            DestinationLockGrabber destinationLockGrabber = new DestinationLockGrabber(input);
+
+            string resultingFrequency = destinationLockGrabber.ResultingFrequency().ToString();
+
+            Console.WriteLine(resultingFrequency);
+        }
+    }
+}
